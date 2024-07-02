b56e9250482506b33ffe7921717ee0d7d9dece58
bharadwaja2000 <nandivada.s@northeastern.edu>
2024-07-02 12:46:59 -0400

    pubs

--- my-react-app 8.33.57 PM/src/Publications.js
+++ my-react-app 8.33.57 PM/src/Publications.js
@@ -2,89 +2,57 @@ import React from 'react';
 
 function Publications() {
 
-  const publications = [
-    {
-        authors: 'Xiong Li , Sangwoo Shim , Katherine R Hardin , Kiran G Vanaja , Hongjun Song, Andre Levchenko , Guo-Li Ming , James Q Zheng 2022',
-        title: 'Signal amplification in growth cone gradient sensing by a double negative feedback loop among PTEN, PI (3, 4, 5) P3 and actomyxosin',
-        journal: 'NIH',
-        date: '2022 Aug 31.',
-        doi: 'https://pubmed.ncbi.nlm.nih.gov/36055521/',
-    },
-    {
-      authors: 'Radha Mukherjee , Kiran G Vanaja , Jacob A Boyer, Sunyana Gadal , Hilla Solomon , Sarat Chandarlapaty , Andre Levchenko , Neal Rosen  2021',
-      title: 'Regulation Of PTEN Translation By PI3K Signaling Maintains Pathway Homeostasis',
-      journal: 'NIH',
-      date: '2022 Aug 31.',
-      doi: 'https://pubmed.ncbi.nlm.nih.gov/33606974/',
-  },
-  {
-    authors: 'Yasir Suhail , Margo P. Cain , Kiran Vanaja Gireesan ,  Paul A. Kurywchak , Andre Levchenko ,  Raghu Kalluri ,and Kshitiz 2019',
-    title: 'Systems Biology Of Cancer Metastasis',
-    journal: 'NIH',
-    date: '2022 Aug 31.',
-    doi: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6716621/',
-},
-{
-  authors: 'Kiran G Vanaja , Winston Timp , Andrew P Feinberg , Andre Levchenko  2018',
-  title: 'A Loss Of Epigenetic Control Can Promote Cell Death Through Reversing The Balance Of Pathways In A Signaling Network',
-  journal: 'NIH',
-  date: '2022 Aug 31.',
-  doi: 'https://pubmed.ncbi.nlm.nih.gov/30244832/',
-},
-{
-  authors: 'Ellen F Foxman , James A Storer , Kiran Vanaja , Andre Levchenko , Akiko Iwasaki  2016',
-  title: 'Two Interferon-Independent Double-Stranded RNA-Induced Host Defense Strategies Suppress The Common Cold Virus At Warm Temperature',
-  journal: 'NIH',
-  date: '2022 Aug 31.',
-  doi: 'https://pubmed.ncbi.nlm.nih.gov/27402752/',
-},
-    
-];
-
-const styles = {
-  container: {
-      textAlign: 'center',
-  },
-  header: {
-      backgroundColor: '#282c34',
-      minHeight: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: 'calc(10px + 2vmin)',
-      color: 'white',
-  },
-  list: {
-      textAlign: 'left',
-      listStyleType: 'none',
-  },
-  listItem: {
-      margin: '10px 0',
-  }
-};
 
 
 
 
   return (
-    <div style={styles.container}>
-    <h1>Publications</h1>
-    <ul style={styles.list}>
-        {publications.map((publication, index) => (
-            <li key={index} style={styles.listItem}>
-                <strong>{publication.authors}</strong> 
-                ({publication.date}). {publication.title}
-                {publication.volume ? <span>{publication.volume}</span> : ''} 
-                {publication.pages ? <span>{publication.pages}</span> : ''}
-                 .{publication.doi ? <a href={`${publication.doi}`}>
-                {publication.doi}</a> : ''} 
-                {/* PMID: {publication.pmid ? <span>{publication.pmid}</span> : ''} 
-                PMCID: {publication.pmcid ? <span>{publication.pmcid}</span> : ''}. */}
-            </li>
-        ))}
-    </ul>
-</div>
+    <div className="publications">
+    <div className="post">
+      <h2 className="title">Signal amplification in growth cone gradient sensing by a double negative feedback loop among PTEN, PI(3,4,5)P3 and actomyosin</h2>
+      <div className="date">August 31, 2022 | NIH</div>
+      <p>
+      Xiong Li<sup>1</sup> , Sangwoo Shim <sup>2</sup>, Katherine R Hardin <sup>3</sup>, Kiran G Vanaja <sup>4</sup>, Hongjun Song <sup>5</sup>, Andre Levchenko <sup>4</sup>, Guo-Li Ming <sup>5</sup>, James Q Zheng <sup>6</sup>
+      </p>
+      <a href='https://pubmed.ncbi.nlm.nih.gov/36055521/' target="_blank"><button className="button">Link→</button></a>
+    </div>
+
+    <div className="post">
+      <h2 className="title">Regulation of PTEN translation by PI3K signaling maintains pathway homeostasis</h2>
+      <div className="date">February 18, 2021 | NIH</div>
+      <p>
+      Radha Mukherjee <sup>1</sup>, Kiran G Vanaja <sup>2</sup>, Jacob A Boyer <sup>1</sup>, Sunyana Gadal <sup>1</sup>, Hilla Solomon <sup>1</sup>, Sarat Chandarlapaty <sup>3</sup>, Andre Levchenko <sup>4</sup>, Neal Rosen <sup>5</sup>
+      </p>
+      <a href='https://pubmed.ncbi.nlm.nih.gov/33606974/' target="_blank"><button className="button">Link→</button></a>
+    </div>
+
+    <div className="post">
+      <h2 className="title">A Loss of Epigenetic Control Can Promote Cell Death through Reversing the Balance of Pathways in a Signaling Network</h2>
+      <div className="date">September 20, 2018 | NIH</div>
+      <p>
+      Kiran G Vanaja <sup>1</sup>, Winston Timp <sup>2</sup>, Andrew P Feinberg <sup>3</sup>, Andre Levchenko <sup>4</sup>
+      </p>
+      <a href='https://pubmed.ncbi.nlm.nih.gov/30244832/' target="_blank"><button className="button">Link→</button></a>
+    </div>
+
+    <div className="post">
+      <h2 className="title">Systems Biology of Cancer Metastasis</h2>
+      <div className="date">August 28, 2019 | NIH</div>
+      <p>
+      Yasir Suhail<sup>1,2,ξ</sup>, Margo P. Cain<sup>3,ξ</sup>, Kiran Vanaja Gireesan<sup>2</sup>, Paul A. Kurywchak<sup>3</sup>, Andre Levchenko <sup>2</sup> Raghu Kalluri <sup>3</sup> and Kshitiz<sup>1,2,*</sup>
+      </p>
+      <a href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6716621/' target="_blank"><button className="button">Link→</button></a>
+    </div>
+
+    <div className="post">
+      <h2 className="title">Two interferon-independent double-stranded RNA-induced host defense strategies suppress the common cold virus at warm temperature</h2>
+      <div className="date">July 26, 2016 | NIH</div>
+      <p>
+      Ellen F Foxman <sup>1</sup>, James A Storer <sup>2</sup>, Kiran Vanaja <sup>3</sup>, Andre Levchenko <sup>3</sup>, Akiko Iwasaki <sup>4</sup>
+      </p>
+      <a href='https://pubmed.ncbi.nlm.nih.gov/27402752/' target="_blank"><button className="button">Link→</button></a>
+    </div>
+  </div>
   );
 }
 
