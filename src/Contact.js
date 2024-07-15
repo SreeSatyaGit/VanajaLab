a7f887fa258ca07e91391f22e208d0504ee6fa62
bharadwaja2000 <nandivada.s@northeastern.edu>
2024-07-15 16:14:40 -0400

    adding contact again

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+// Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h1>Get in Touch</h1>
+      <p>
+        Contact us to learn more about our research and collaborations.
+        k.vanaja@northeastern.edu
+        
+      </p>
+    </section>
+  );
+}
+
+export default Contact;
